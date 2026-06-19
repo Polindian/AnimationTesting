fcb5fe369c5e97068e3512227767c25fd9c701cc
polindian <christopher.naglik@hotmail.com>
2026-06-19 16:02:29 +0100

    Added ChrisGameState with replicated player slot selection system
    - ChrisGameState holds a replicated TArray<FPlayerSelection> to track hwich player is in which lobby slot
    - RequestPlayerSelectionChange validates validates on server (authority + occupied checks) before updating
    - ReplicatedUsing/OnRep used to auto push slot changes to all clients
    - OnPlayerSelectionUpdated delegate notifies UI widgets on server and client when the array changes so it loops through
    - Integrates with FPlayerSelection struct for player identity and slot data
    - Next steps: not testable yet, need to connect LobbyWidget with replicated Slot Selection System

---
+++ Source/AnimationTesting/Private/Framework/ChrisGameState.cpp
@@ -0,0 +1,58 @@
+﻿// Christopher Naglik All Rights Reserved
+
+
+#include "Framework/ChrisGameState.h"
+#include "Net/UnrealNetwork.h"
+
+void AChrisGameState::RequestPlayerSelectionChange(const APlayerState* RequestingPlayer, uint8 DesiredSlot)
+{
+	if(!HasAuthority() || IsSlotOccupied(DesiredSlot))
+	{
+		return;
+	}
+
+	FPlayerSelection* PlayerSelectionPtr = PlayerSelectionArray.FindByPredicate([&] (const FPlayerSelection& PlayerSelection)
+	{
+		return PlayerSelection.IsForPlayer(RequestingPlayer);
+		});
+
+	if (PlayerSelectionPtr)
+	{
+		PlayerSelectionPtr->SetSlot(DesiredSlot);
+	}
+	else
+	{
+		PlayerSelectionArray.Add(FPlayerSelection(DesiredSlot, RequestingPlayer));
+	}
+
+	OnPlayerSelectionUpdated.Broadcast(PlayerSelectionArray);
+}
+
+bool AChrisGameState::IsSlotOccupied(uint8 SlotId) const
+{
+	for(const FPlayerSelection& PlayerSelection : PlayerSelectionArray)
+	{
+		if(PlayerSelection.GetPlayerSlot() == SlotId)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void AChrisGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(AChrisGameState, PlayerSelectionArray, COND_None, REPNOTIFY_Always);
+}
+
+const TArray<FPlayerSelection>& AChrisGameState::GetPlayerSelection() const
+{
+	return PlayerSelectionArray;
+}
+
+void AChrisGameState::OnRep_PlayerSelectionArray()
+{
+	OnPlayerSelectionUpdated.Broadcast(PlayerSelectionArray);
+}
---
+++ Source/AnimationTesting/Private/Framework/ChrisGameState.h
@@ -0,0 +1,35 @@
+﻿// Christopher Naglik All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Player/PlayerInfoTypes.h"
+#include "ChrisGameState.generated.h"
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerSelectionUpdated, const TArray<FPlayerSelection>& /*NewPlayerSelection*/)
+
+/**
+ * 
+ */
+UCLASS()
+class AChrisGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+public:
+	void RequestPlayerSelectionChange(const APlayerState* RequestingPlayer, uint8 DesiredSlot);
+	bool IsSlotOccupied(uint8 SlotId) const;
+
+	FOnPlayerSelectionUpdated OnPlayerSelectionUpdated;
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	const TArray<FPlayerSelection>& GetPlayerSelection() const;
+
+private:
+	UPROPERTY(ReplicatedUsing = OnRep_PlayerSelectionArray)
+	TArray<FPlayerSelection> PlayerSelectionArray;
+
+	UFUNCTION()
+	void OnRep_PlayerSelectionArray();
+};
