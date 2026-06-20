93db644e1f881feb4067313fcde969d566eac591
polindian <christopher.naglik@hotmail.com>
2026-06-20 18:11:46 +0100

    Start of setup for hero selection page
    - Made a PrimaryDataAsset in order to store a hero's meta data (display name, icon (screenshot - to do later), character class, preview anim BP)
    - Provides synchronous loading for resolving soft pointers so preview runs smoothly and no memory is wasted

---
+++ Source/AnimationTesting/Private/Character/PA_CharacterDefinition.cpp
@@ -0,0 +1,57 @@
+﻿// Christopher Naglik All Rights Reserved
+
+
+#include "Character/PA_CharacterDefinition.h"
+#include "Character/ChrisCharacter.h"
+
+FPrimaryAssetId UPA_CharacterDefinition::GetPrimaryAssetId() const
+{
+	return FPrimaryAssetId(GetCharacterDefinitionAssetType(), GetFName());
+}
+
+FPrimaryAssetType UPA_CharacterDefinition::GetCharacterDefinitionAssetType()
+{
+	return FPrimaryAssetType("Character Definition");
+}
+
+UTexture2D* UPA_CharacterDefinition::LoadIcon() const
+{
+	CharacterIcon.LoadSynchronous();
+	if (CharacterIcon.IsValid())
+	{
+		return CharacterIcon.Get();
+	}
+	return nullptr;
+}
+
+TSubclassOf<AChrisCharacter> UPA_CharacterDefinition::LoadCharacterClass() const
+{
+	CharacterClass.LoadSynchronous();
+	if (CharacterClass.IsValid())
+	{
+		return CharacterClass.Get();
+	}
+
+	return TSubclassOf<AChrisCharacter>();
+}
+
+TSubclassOf<UAnimInstance> UPA_CharacterDefinition::LoadDisplayAnimationBP() const
+{
+	DisplayAnimBP.LoadSynchronous();
+	if (DisplayAnimBP.IsValid()) {
+		return DisplayAnimBP.Get();
+	}
+	
+	return TSubclassOf<UAnimInstance>();
+}
+
+USkeletalMesh* UPA_CharacterDefinition::LoadDisplayMesh() const
+{
+	TSubclassOf<AChrisCharacter> LoadedCharacterClass = LoadCharacterClass();
+	if (!LoadedCharacterClass) return nullptr;
+
+	ACharacter* Character = Cast<ACharacter>(LoadedCharacterClass.GetDefaultObject());
+	if (!Character) return nullptr;
+
+	return Character->GetMesh()->GetSkeletalMeshAsset();
+}
