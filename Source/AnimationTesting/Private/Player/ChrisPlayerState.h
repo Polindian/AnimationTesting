2456bf21da28202d5f0ac2996735147c96d006aa
polindian <christopher.naglik@hotmail.com>
2026-06-23 17:11:58 +0100

    Added to ChrisPlayerState for per-player character selection and hero selection infrastructure
    - Stores local replicated copy of FPlayerSelection on each player's PlayerState
    - Subscribes to GameState's OnPlayerSelectionUpdated to keep local copy in sync
    - Sever_SetSelectedCharacterDefinition RPC deselects older character, selects new one via GameState
    - PlayerState persists beyond lobby for use inspawning/gameplay systems

--- Source/AnimationTesting/Private/Player/ChrisPlayerState.h
+++ Source/AnimationTesting/Private/Player/ChrisPlayerState.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "Player/PlayerInfoTypes.h"
 #include "ChrisPlayerState.generated.h"
+
+class UPA_CharacterDefinition;
 
 /**
  * 
@@ -14,4 +17,25 @@ class AChrisPlayerState : public APlayerState
 {
 	GENERATED_BODY()
 	
+public:
+	AChrisPlayerState();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void BeginPlay() override;
+
+
+	// Server RPC: client tells the server which character they've picked.
+   // The PlayerState then forwards this to ChrisGameState.
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetSelectedCharacterDefinition(const UPA_CharacterDefinition* NewDefinition);
+
+private:
+	// Server RPC: client tells the server which character they've picked.
+   // The PlayerState then forwards this to ChrisGameState.
+	UPROPERTY(Replicated)
+	FPlayerSelection PlayerSelection;
+
+	UPROPERTY()
+	class AChrisGameState* ChrisGameState;
+
+	void PlayerSelectionUpdated(const TArray<FPlayerSelection>& NewPlayerSelections);
 };
