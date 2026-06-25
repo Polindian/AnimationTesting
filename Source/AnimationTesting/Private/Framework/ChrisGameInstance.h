02e25440599ad66ad86dfe5a09ce3eb7be95608c
polindian <christopher.naglik@hotmail.com>
2026-06-25 11:40:17 +0100

    Added custom ChrisGameInstance with server travel support for maps
    Created UChrisGameInstance with soft object references for different maps
    - Implemented 'StartMatch()' gaited to server-only net mode, hero selection logic will follow after
    - Added LoadLevelAndListen() helper to resolve soft level references and performs a listening ServerTravel

---
+++ Source/AnimationTesting/Private/Framework/ChrisGameInstance.h
@@ -0,0 +1,31 @@
+﻿// Christopher Naglik All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ChrisGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UChrisGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	void StartMatch();
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> MainMenuLevel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> LobbyLevel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Map")
+	TSoftObjectPtr<UWorld> Lvl_ThirdPerson;
+
+	void LoadLevelAndListen(TSoftObjectPtr<UWorld>Level);
+};
