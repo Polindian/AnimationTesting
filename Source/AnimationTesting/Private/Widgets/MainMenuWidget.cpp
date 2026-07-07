65b558d0ab714b026b2a7aed0c392c231d11d1d4
polindian <christopher.naglik@hotmail.com>
2026-07-07 21:13:59 +0100

    Set up Main Menu for Login
    -Added MainMenuGameMode
    -Added MainMenuPlayerController (same settings with UI only)
    - Made new blank level for Main Menu (end product will be video in background behind widgets)

---
+++ Source/AnimationTesting/Private/Widgets/MainMenuWidget.cpp
@@ -0,0 +1,8 @@
+// Christopher Naglik All Rights Reserved
+
+
+#include "Widgets/MainMenuWidget.h"
+
+
+
+
---
+++ Source/AnimationTesting/Private/Widgets/MainMenuWidget.h
@@ -0,0 +1,20 @@
+// Christopher Naglik All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMainMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
